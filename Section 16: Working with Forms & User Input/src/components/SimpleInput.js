7d189cf57825dfa98eea5cf76630ea369c41677b
Gabor Zay <gaborzay@gmail.com>
2022-02-04 13:57:00 -0800

    Removing some dead code.

--- Section 16: Working with Forms & User Input/src/components/SimpleInput.js
+++ Section 16: Working with Forms & User Input/src/components/SimpleInput.js
@@ -1,4 +1,3 @@
-// import { useEffect, useRef, useState } from 'react'
 import useInput from '../hooks/use-input'
 
 const SimpleInput = (props) => {
@@ -18,101 +17,40 @@ const SimpleInput = (props) => {
     inputChangeHandler: emailChangeHandler,
     inputBlurHandler: emailBlurHandler,
     reset: resetEmailInput,
-  } = useInput(value => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value))
-
-  // const nameInputRef = useRef() // Only grabs when needed
-  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
-  // const [formIsValid, setFormIsValid] = useState(false)
-  // const [enteredName, setEnteredName] = useState('') // Updates every key
-  // stroke const [enteredNameTouched, setEnteredNameTouched] = useState(false)
-
-  // Updates every keystroke
-  // const [enteredEmail, setEnteredEmail] = useState('')
-  // const [enteredEmailTouched, setEnteredEmailTouched] = useState(false)
-
-  // const enteredNameIsValid = enteredName.trim() !== ''
-  // const nameInputIsValid = !enteredNameIsValid && enteredNameTouched
-
-  // const enteredEmailIsValid =
-  // /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(enteredEmail) const
-  // emailInputIsValid = !enteredEmailIsValid && enteredEmailTouched
+  } = useInput(
+    value => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)
+  )
 
   let formIsValid = false
 
-  // useEffect(() => {
   if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true
   }
-  // }, [enteredNameIsValid])
-
-  // useEffect(() => {
-  //   if (enteredNameIsValid) {
-  //     console.log('Name Input is valid!')
-  //   }
-  // }, [enteredNameIsValid])
-
-  // const nameInputChangeHandler = event => {
-  //   setEnteredName(event.target.value)
-  //
-  //   if (event.target.value.trim() !== '') {
-  //     setEnteredNameIsValid(true)
-  //     return
-  //   }
-  // }
-
-  // const nameInputBlurHandler = event => {
-  //   setEnteredNameTouched(true)
-  //
-  //   if (enteredName.trim() === '') {
-  //     setEnteredNameIsValid(false)
-  //     return
-  //   }
-  // }
-
-  // const emailInputChangeHandler = event => {
-  //   setEnteredEmail(event.target.value)
-  // }
-
-  // const emailInputBlurHandler = event => {
-  //   setEnteredEmailTouched(true)
-  // }
 
   const formSubmissionHandler = event => {
     event.preventDefault()
 
-    // if (enteredName.trim() === '') {
-    //   setEnteredNameIsValid(false)
     if (!enteredNameIsValid || !enteredEmailIsValid) {
       return
     }
 
-    // setEnteredNameIsValid(true)
-    // console.log(enteredName)
-
-    // If we want to set value to empty string
-    // setEnteredName('')
-    // setEnteredNameTouched(false)
-    // setEnteredEmail('')
-    // setEnteredEmailTouched(false)
     resetNameInput()
     resetEmailInput()
-
-    // const enteredValue = nameInputRef.current.value
-    // console.log(enteredValue)
-    // Not ideal, should not manipulate the dom
-    // nameInputRef.current.value = ''
   }
 
-  const inputClasses = nameInputHasError && emailInputHasError
+  const inputNameClasses = nameInputHasError
+    ? 'form-control invalid'
+    : 'form-control'
+
+  const inputEmailClasses = emailInputHasError
     ? 'form-control invalid'
     : 'form-control'
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className={inputClasses}>
+      <div className={inputNameClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          // ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameChangeHandler}
@@ -121,9 +59,10 @@ const SimpleInput = (props) => {
         {nameInputHasError && (
           <p className="error-text">Name must not be empty.</p>
         )}
+      </div>
+      <div className={inputEmailClasses}>
         <label htmlFor="email">Your Email</label>
         <input
-          // ref={nameInputRef}
           type="email"
           id="email"
           onChange={emailChangeHandler}
