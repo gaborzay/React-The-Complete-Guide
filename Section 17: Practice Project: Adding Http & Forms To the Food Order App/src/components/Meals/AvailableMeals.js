c6368ba8b9833a1673a9febd092ebfef7a8c8b6a
Gabor Zay <gaborzay@gmail.com>
2022-02-18 16:27:37 -0800

    Added Section 17: Adding http to forms.

---
+++ Section 17: Practice Project: Adding Http & Forms To the Food Order App/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,76 @@
+import Card from '../UI/Card'
+import MealItem from './MealItem/MealItem'
+import classes from './AvailableMeals.module.css'
+import { useEffect, useState } from 'react'
+
+const BASE_URL = 'https://react-http-2f56a-default-rtdb.firebaseio.com/'
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [httpError, setHttpError] = useState()
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true)
+      const response = await fetch(`${BASE_URL}/meals.json`)
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+
+      const responseData = await response.json()
+
+      const loadedMeals = []
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key]['name'],
+          description: responseData[key]['description'],
+          price: responseData[key]['price'],
+        })
+      }
+
+      setMeals(loadedMeals)
+    }
+
+    fetchMeals().catch(error => {
+      setHttpError(error.message)
+    }).finally(() => {
+      setIsLoading(false)
+    })
+  }, [])
+
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>
+      <p>Loading....</p>
+    </section>
+  }
+
+  if (httpError) {
+    return <section className={classes.MealsError}>
+      <p>{httpError}.</p>
+    </section>
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ))
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  )
+}
+
+export default AvailableMeals
